c40f6ca35fedac631224be20f9ab5d8ff9d63627
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-06-01 19:27:20 +0300

    add GetToDoItemcursorPaginatedListQuery (request)

---
+++ ToDoList.Core/Features/ToDoItem/Queries/models/GetToDoItemcursorPaginatedListQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using ToDoList.Core.Features.ToDoItem.Queries.Result;
+using ToDoList.Core.Wrappers;
+
+namespace ToDoList.Core.Features.ToDoItem.Queries.models
+{
+    public class GetToDoItemcursorPaginatedListQuery : IRequest<CursorPaginatedResult<GetToDoItemCursorPaginatedListResponse>>
+    {
+        public int Cursor { get; set; }
+        public int PageSize { get; set; }
+    }
+}
