387f1395bef653c6a182822dea54961b94a031cd
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-06-01 18:15:16 +0300

    Add cursor PaginationResult

---
+++ ToDoList.Core/Wrappers/CursorPaginatedResult.cs
@@ -0,0 +1,27 @@
+﻿namespace ToDoList.Core.Wrappers
+{
+    public class CursorPaginatedResult<T>
+    {
+        public CursorPaginatedResult(bool succeeded, List<T> data, string nextCursor, string previousCursor, List<string> messages = null)
+        {
+            Succeeded = succeeded;
+            Data = data;
+            NextCursor = nextCursor;
+            PreviousCursor = previousCursor;
+            Messages = messages ?? new List<string>();
+        }
+
+        public bool Succeeded { get; set; }
+        public List<T> Data { get; set; }
+        public string NextCursor { get; set; }
+        public string PreviousCursor { get; set; }
+        public List<string> Messages { get; set; }
+
+        public static CursorPaginatedResult<T> Success(List<T> data, string nextCursor, string previousCursor)
+        {
+            return new CursorPaginatedResult<T>(true, data, nextCursor, previousCursor);
+        }
+
+    }
+
+}
