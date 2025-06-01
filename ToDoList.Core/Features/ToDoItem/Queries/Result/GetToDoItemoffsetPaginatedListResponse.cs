5c2c83f9dd915537309c341cdbe42db4089bbae6
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-06-01 17:29:49 +0300

    make response

--- ToDoList.Core/Features/ToDoItem/Queries/Result/GetToDoItemoffsetPaginatedListResponse.cs
+++ ToDoList.Core/Features/ToDoItem/Queries/Result/GetToDoItemoffsetPaginatedListResponse.cs
@@ -2,5 +2,18 @@
 {
     public class GetToDoItemoffsetPaginatedListResponse
     {
+        public int Id { get; set; }
+
+
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public GetToDoItemoffsetPaginatedListResponse(int id, string title, bool iscompleted)
+        {
+            Id = id;
+            Title = title;
+            IsCompleted = iscompleted;
+        }
     }
 }
