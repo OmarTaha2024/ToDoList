f843d3a3ec56ed1eecfb3243be209cec826eba5c
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-06-01 19:27:55 +0300

    add GetToDoItemCursorPaginatedListResponse

--- ToDoList.Core/Features/ToDoItem/Queries/Result/GetToDoItemCursorPaginatedListResponse.cs
+++ ToDoList.Core/Features/ToDoItem/Queries/Result/GetToDoItemCursorPaginatedListResponse.cs
@@ -2,5 +2,18 @@
 {
     public class GetToDoItemCursorPaginatedListResponse
     {
+        public int Id { get; set; }
+
+
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public GetToDoItemCursorPaginatedListResponse(int id, string title, bool iscompleted)
+        {
+            Id = id;
+            Title = title;
+            IsCompleted = iscompleted;
+        }
     }
 }
